53d2cda1b1248fc62feb3e02c1f87ed75dd09491
Manuel Bieh <github@manuelbieh.de>
2011-08-24 09:07:29 -0700

    Added Vincenty inverse formula for more accurate results (old getDistance() function was renamed to getDistanceSimple()).

--- geolib.js
+++ geolib.js
@@ -5,10 +5,11 @@
  * 
  * @author Manuel Bieh
  * @url http://www.manuel-bieh.de/
- * @version 1.1.2
+ * @version 1.1.3
  * @license http://www.gnu.org/licenses/lgpl-3.0.txt LGPL
  *
  */
+
 ;(function (window, undefined) {
 
 	var radius = 6378137 // Earth radius
@@ -25,23 +26,166 @@
 		distance: 0,
 
 		/**
-		 * Calculates the distance between two spots
+		 * Calculates geodetic distance between two points specified by latitude/longitude using 
+		 * Vincenty inverse formula for ellipsoids
+		 * Vincenty Inverse Solution of Geodesics on the Ellipsoid (c) Chris Veness 2002-2010
+		 * CC BY 3.0
 		 *
 		 * @param    object    Start position {latitude: 123, longitude: 123}
 		 * @param    object    End position {latitude: 123, longitude: 123}
 		 * @param    integer   Accuracy (in meters)
 		 * @return   integer   Distance (in meters)
 		 */
+
 		getDistance: function(start, end, accuracy) {
 
 			accuracy = parseInt(accuracy) || 1;
 
 			var coord1 = {}, coord2 = {};
-			coord1.latitude = (geolib.useDecimal(start.latitude)/ 180 * Math.PI);
-			coord1.longitude = (geolib.useDecimal(start.longitude)/ 180 * Math.PI);
+			coord1.latitude = geolib.useDecimal(start.latitude);
+			coord1.longitude = geolib.useDecimal(start.longitude);
 
-			coord2.latitude = (geolib.useDecimal(end.latitude)/ 180 * Math.PI);
-			coord2.longitude = (geolib.useDecimal(end.longitude)/ 180 * Math.PI);
+			coord2.latitude = geolib.useDecimal(end.latitude);
+			coord2.longitude = geolib.useDecimal(end.longitude);
+
+			var a = 6378137, b = 6356752.314245,  f = 1/298.257223563;  // WGS-84 ellipsoid params
+			var L = (coord2.longitude-coord1.longitude).toRad();
+
+			var U1 = Math.atan((1-f) * Math.tan(parseFloat(coord1.latitude).toRad()));
+			var U2 = Math.atan((1-f) * Math.tan(parseFloat(coord2.latitude).toRad()));
+			var sinU1 = Math.sin(U1), cosU1 = Math.cos(U1);
+			var sinU2 = Math.sin(U2), cosU2 = Math.cos(U2);
+
+			var lambda = L, lambdaP, iterLimit = 100;
+			do {
+				var sinLambda = Math.sin(lambda), cosLambda = Math.cos(lambda);
+				var sinSigma = (
+					Math.sqrt(
+						(
+							cosU2 * sinLambda
+						) * (
+							cosU2 * sinLambda
+						) + (
+							cosU1 * sinU2 - sinU1 * cosU2 * cosLambda
+						) * (
+							cosU1 * sinU2 - sinU1 * cosU2 * cosLambda
+						)
+					)
+				);
+				if (sinSigma==0) {
+					return geolib.distance = 0;  // co-incident points
+				}
+				var cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
+				var sigma = Math.atan2(sinSigma, cosSigma);
+				var sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+				var cosSqAlpha = 1 - sinAlpha * sinAlpha;
+				var cos2SigmaM = cosSigma - 2 * sinU1 * sinU2 / cosSqAlpha;
+				if (isNaN(cos2SigmaM)) {
+					cos2SigmaM = 0;  // equatorial line: cosSqAlpha=0 (§6)
+				}
+				var C = (
+					f / 16 * cosSqAlpha * (
+						4 + f * (
+							4 - 3 * cosSqAlpha
+						)
+					)
+				);
+				lambdaP = lambda;
+				lambda = (
+					L + (
+						1 - C
+					) * f * sinAlpha * (
+						sigma + C * sinSigma * (
+							cos2SigmaM + C * cosSigma * (
+								-1 + 2 * cos2SigmaM * cos2SigmaM
+							)
+						)
+					)
+				);
+
+			} while (Math.abs(lambda-lambdaP) > 1e-12 && --iterLimit>0);
+
+			if (iterLimit==0) {
+				return NaN  // formula failed to converge
+			}
+
+			var uSq = (
+				cosSqAlpha * (
+					a * a - b * b
+				) / (
+					b*b
+				)
+			);
+
+			var A = (
+				1 + uSq / 16384 * (
+					4096 + uSq * (
+						-768 + uSq * (
+							320 - 175 * uSq
+						)
+					)
+				)
+			);
+
+			var B = (
+				uSq / 1024 * (
+					256 + uSq * (
+						-128 + uSq * (
+							74-47 * uSq
+						)
+					)
+				)
+			);
+
+			var deltaSigma = (
+				B * sinSigma * (
+					cos2SigmaM + B / 4 * (
+						cosSigma * (
+							-1 + 2 * cos2SigmaM * cos2SigmaM
+						) -B / 6 * cos2SigmaM * (
+							-3 + 4 * sinSigma * sinSigma
+						) * (
+							-3 + 4 * cos2SigmaM * cos2SigmaM
+						)
+					)
+				)
+			);
+
+			var distance = b * A * (sigma - deltaSigma);
+
+			distance = distance.toFixed(3); // round to 1mm precision
+			return geolib.distance = parseInt(Math.round(distance/accuracy)*accuracy)
+
+			/*
+			// note: to return initial/final bearings in addition to distance, use something like:
+			var fwdAz = Math.atan2(cosU2*sinLambda,  cosU1*sinU2-sinU1*cosU2*cosLambda);
+			var revAz = Math.atan2(cosU1*sinLambda, -sinU1*cosU2+cosU1*sinU2*cosLambda);
+
+			return { distance: s, initialBearing: fwdAz.toDeg(), finalBearing: revAz.toDeg() };
+			*/
+
+		},
+
+
+		/**
+		 * Calculates the distance between two spots. 
+		 * This method is more simple but also more inaccurate
+		 *
+		 * @param    object    Start position {latitude: 123, longitude: 123}
+		 * @param    object    End position {latitude: 123, longitude: 123}
+		 * @param    integer   Accuracy (in meters)
+		 * @return   integer   Distance (in meters)
+		 */
+		getDistanceSimple: function(start, end, accuracy) {
+
+			accuracy = parseInt(accuracy) || 1;
+
+			var coord1 = {}, coord2 = {};
+			coord1.latitude = parseFloat(geolib.useDecimal(start.latitude)).toRad();
+			coord1.longitude = parseFloat(geolib.useDecimal(start.longitude)).toRad();
+
+			coord2.latitude = parseFloat(geolib.useDecimal(end.latitude)).toRad();
+			coord2.longitude = parseFloat(geolib.useDecimal(end.longitude)).toRad();
 
 			var distance = 
 				Math.round(
@@ -358,6 +502,12 @@
 			return Math.round(value * decPlace)/decPlace;
 		}
 
+	}
+
+	if (typeof(Number.prototype.toRad) === "undefined") {
+		Number.prototype.toRad = function() {
+			return this * Math.PI / 180;
+		}
 	}
 
 	window.geolib = geolib;
