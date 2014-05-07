483321802e800dd197604a15e48c5c457b9b1077
Manuel Bieh <github@manuelbieh.de>
2014-05-07 12:34:09 +0200

    struggling with npm

--- dist/geolib.elevation.js
+++ dist/geolib.elevation.js
@@ -1,10 +1,10 @@
-/*! geolib.elevation 2.0.9 by Manuel Bieh
+/*! geolib.elevation 2.0.10 by Manuel Bieh
 *
 * Elevation Addon for Geolib.js
 * 
 * @author Manuel Bieh
 * @url http://www.manuelbieh.com/
-* @version 2.0.9
+* @version 2.0.10
 * @license MIT
 */
 ;(function(global, geolib, undefined) {
