a3795eec80be37a82a3d6d9bb8cdab70460c3cf1
Manuel Bieh <github@manuelbieh.de>
2014-05-07 11:30:26 +0200

    removed bin property from package.json

--- dist/geolib.elevation.js
+++ dist/geolib.elevation.js
@@ -1,10 +1,10 @@
-/*! geolib.elevation 2.0.7 by Manuel Bieh
+/*! geolib.elevation 2.0.8 by Manuel Bieh
 *
 * Elevation Addon for Geolib.js
 * 
 * @author Manuel Bieh
 * @url http://www.manuelbieh.com/
-* @version 2.0.7
+* @version 2.0.8
 * @license MIT
 */
 ;(function(global, geolib, undefined) {
