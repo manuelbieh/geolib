0406fbd120cb371a89a1676e47e9de5f0e51a4a5
Manuel <github@manuelbieh.de>
2016-05-06 22:08:24 +0200

    version bump

--- dist/geolib.elevation.js
+++ dist/geolib.elevation.js
@@ -1,10 +1,10 @@
-/*! geolib.elevation 2.0.20 by Manuel Bieh
+/*! geolib.elevation 2.0.21 by Manuel Bieh
 *
 * Elevation Addon for Geolib.js
 * 
 * @author Manuel Bieh
 * @url http://www.manuelbieh.com/
-* @version 2.0.20
+* @version 2.0.21
 * @license MIT
 */
 ;(function(global, geolib, undefined) {
