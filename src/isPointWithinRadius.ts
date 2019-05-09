3ca95a52c26cfccdf08fb64d613d852b2d864e2f
Manuel Bieh <info@manuelbieh.de>
2019-05-10 00:51:34 +0200

    fixed incorrect import

--- src/isPointWithinRadius.ts
+++ src/isPointWithinRadius.ts
@@ -1,5 +1,5 @@
-import { GeolibInputCoordinates } from 'types';
 import getDistance from './getDistance';
+import { GeolibInputCoordinates } from './types';
 
 // Checks if a point is inside of given radius
 const isPointWithinRadius = (
