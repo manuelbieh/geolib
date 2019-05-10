51ca4df063e7cd6f3a5faa9f806d42098190a4d3
Manuel Bieh <info@manuelbieh.de>
2019-05-11 01:09:17 +0200

    re-added and fixed decimalToSexagesimal, added tests, renamed test.ts files to test.js

--- src/getSpeed.test.js
+++ src/getSpeed.test.js
@@ -8,15 +8,6 @@ describe('getSpeed', () => {
                 { lat: 52.54944, lng: 13.468509, time: 1360245600880 }
             )
         ).toEqual(29.86777777777778);
-
-        console.log(
-            getSpeed(
-                { lat: 51.567294, lng: 7.38896, time: 1360231200880 },
-                { lat: 52.54944, lng: 13.468509, time: 1360245600880 }
-            )
-        );
-        // equal(speedInKMH, 107.7308, '430.923 km in 4 hours should equal 107.7308 kmh');
-        // equal(speedInMPH, 66.9408, '430.923 km in 4 hours should equal 66.9408 mph');
     });
 
     it('uses an alternative getDistance function if one is passed', () => {
