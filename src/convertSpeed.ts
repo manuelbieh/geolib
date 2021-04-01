ea46373cba6c50a6c83cf962282ef70b995228a5
Marco Weber <49274286+mrcwbr@users.noreply.github.com>
2021-04-01 10:33:36 +0200

    Improved types of targetUnit in convertSpeed.ts

--- src/convertSpeed.ts
+++ src/convertSpeed.ts
@@ -2,7 +2,7 @@ import { distanceConversion, timeConversion } from './constants';
 
 // This is a convenience function to easily convert the result of getSpeed()
 // into miles per hour or kilometers per hour.
-const convertSpeed = (metersPerSecond: number, targetUnit = 'kmh') => {
+const convertSpeed = (metersPerSecond: number, targetUnit: 'kmh' | 'mph' = 'kmh') => {
     switch (targetUnit) {
         case 'kmh':
             return metersPerSecond * timeConversion.h * distanceConversion.km;
