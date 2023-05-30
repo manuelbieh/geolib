955937b6a0ec53a9ced3667c923ab413a31eb8c6
Darren Smith <ddarrensmith@gmail.com>
2023-05-29 19:34:34 -0500

    fix: getRoughCompassDirection regex used "contains" logic rather than exact matching resulting incorrect results

--- src/getRoughCompassDirection.ts
+++ src/getRoughCompassDirection.ts
@@ -1,19 +1,19 @@
 // Receives an exact compass direction (like WNW) and spits out a very rough
 // and overly simplified direction (N|E|S|W). Use with caution!
 const getRoughCompassDirection = (exact: string) => {
-    if (/^NNE|NE|NNW|N$/.test(exact)) {
+    if (/^(NNE|NE|NNW|N)$/.test(exact)) {
         return 'N';
     }
 
-    if (/^ENE|E|ESE|SE$/.test(exact)) {
+    if (/^(ENE|E|ESE|SE)$/.test(exact)) {
         return 'E';
     }
 
-    if (/^SSE|S|SSW|SW$/.test(exact)) {
+    if (/^(SSE|S|SSW|SW)$/.test(exact)) {
         return 'S';
     }
 
-    if (/^WSW|W|WNW|NW$/.test(exact)) {
+    if (/^(WSW|W|WNW|NW)$/.test(exact)) {
         return 'W';
     }
 };
